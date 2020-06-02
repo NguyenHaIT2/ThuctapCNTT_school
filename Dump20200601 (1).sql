8091c6241148829ff3b39b549d1a44d4d44d5190
phamlehuy53 <unknownsol98@gmail.com>
2020-06-02 08:58:40 +0700

    Downgrade SDK version to 1.8 (easy to use)

--- Dump20200601 (1).sql
+++ Dump20200601 (1).sql
@@ -4,6 +4,9 @@
 -- ------------------------------------------------------
 -- Server version	8.0.19
 
+create schema school;
+USE school;
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
